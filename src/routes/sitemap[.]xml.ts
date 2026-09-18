50d2283f333854b5aab0c9f6eab5e06e3b053b33
Dfrenzy5 <166166570+Dfrenzy5@users.noreply.github.com>
2026-09-18 08:02:55 +0100

    Update sitemap[.]xml.ts
    Connect sitemap to journal data

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 import { CASE_STUDIES } from "@/data/case-studies";
 import { SITE_URL } from "@/lib/site";
+import { JOURNAL_ENTRIES } from "@/data/journal";
 
 
 
